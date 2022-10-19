eee5ca6b3ca276f6570f09ed318c1b34a3e30958
jlbbacs <josephlestebacs@gmail.com>
2022-10-19 09:50:36 +0800

    channging celsius

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
     
     <div className="weather-data">
         <p className="city">{weatherData.name}</p>
-        <p className="temp">{Math.round(weatherData.main.temp)}°C</p>
+        <p className="temp">{Math.round(weatherData.main.temp)}°F</p>
         <p className="weather">{weatherData.weather[0].main}</p>
 
     </div>
